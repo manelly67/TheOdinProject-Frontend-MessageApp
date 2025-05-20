ed9c51ac249bd0dd45987c6fd5f170b2d235d30b
manellylopez <manellylopezqqq@gmail.com>
2025-05-19 20:10:45 -0500

    testing links behaviour

--- tests/Navbar.test.jsx
+++ tests/Navbar.test.jsx
@@ -2,7 +2,9 @@ import { describe, it, expect } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Navbar from "../src/components/Navbar";
-import { MemoryRouter } from "react-router-dom";
+import SingUp from "../src/components/SignUp";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import Login from "../src/components/Login";
 
 describe("Navbar small screen", () => {
   it("small screen", async () => {
@@ -10,7 +12,7 @@ describe("Navbar small screen", () => {
 
     render(
       <MemoryRouter initialEntries={["/main_app"]}>
-        <Navbar screenWidth={390} />
+        <Navbar screenWidth={390} token={null} />
       </MemoryRouter>
     );
     // in small screen get a button instead a navbar
@@ -21,5 +23,35 @@ describe("Navbar small screen", () => {
     // when click the button navbar appears
     await user.click(button);
     expect(screen.queryByRole("navigation")).toBeInTheDocument();
+  });
+
+  it("simulates routing behavior - Sign Up", async () => {
+    const user = userEvent.setup();
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/" element={<Navbar screenWidth={900} token={null} />} />
+          <Route path="/sign_up" element={<SingUp />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const sign_up = screen.getByRole("link", { name: /sign up/i });
+    await user.click(sign_up);
+    expect(screen.getByText(/Sign Up/)).toBeInTheDocument();
+  });
+
+  it("simulates routing behavior - Login", async () => {
+    const user = userEvent.setup();
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/" element={<Navbar screenWidth={900} token={null} />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const login = screen.getByRole("link", { name: /login/i });
+    await user.click(login);
+    expect(screen.getByText(/Login/)).toBeInTheDocument();
   });
 });
